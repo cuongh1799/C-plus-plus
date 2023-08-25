e526130ae6cf418234071c65a425ca3533c645ae
cuongh1799 <cuongghuynhh1009@gmail.com>
2023-08-25 13:46:09 +0700

    i

--- things/test.cpp
+++ things/test.cpp
@@ -1,18 +1,79 @@
 #include <iostream>
-#include <string>
 #include <vector>
-#include <fstream>
-#include <sstream>
+using namespace std;
 
-int main(){
-	std::vector<int> vec;
+class Book {
+private:
+    string title;
+    int availableCopies;
+    vector<string> namesOfBorrowers;
 
-	vec.push_back(6);
-	vec.push_back(-17);
-	vec.push_back(12);
+public:
+    Book()
+    {
+    }
+    Book(string title, int availableCopies, vector<string> namesOfBorrowers) : title(title), availableCopies(availableCopies), namesOfBorrowers(namesOfBorrowers)
+    {
+    }
+    friend class User;
+};
 
-	
-	std::cout << vec[1];
+class User{
+private:
+    string name;
+    vector<Book *> borrowedBooks;
+
+public:
+    User()
+    {
+    }
+    User(string name, vector<Book *> borrowedBooks) : name(name), borrowedBooks(borrowedBooks)
+    {
+    }
+    bool doBorrow(Book &abook)
+    {
+        if (abook.availableCopies == 0)
+        {
+            cout << "No more copy";
+            return false;
+        }
+        else
+        {
+            borrowedBooks.push_back(&abook);
+            abook.namesOfBorrowers.push_back(name);
+            abook.availableCopies--;
+            return true;
+        }
+    }
+
+	int lenght = borrowedBooks.size();
+    void doReturn(Book &abook) {
+        for (int i = 0; i < borrowedBooks.size(); i++) {
+            if (borrowedBooks[i]->title.compare(abook.title)==0) {
+                for (int j = 0; j < abook.namesOfBorrowers.size(); j++) {
+                    if (abook.namesOfBorrowers[j].compare(name) == 0) {
+                        borrowedBooks.erase(borrowedBooks.begin() + i);
+                        abook.namesOfBorrowers.erase(abook.namesOfBorrowers.begin() + j);
+                        abook.availableCopies++;
+                        cout << "Book: " << abook.title << " has returned" << endl;
+                    }
+                    else {
+                        cout << "Name is not match " << endl;
+                    }
+                }
+            }
+            else {
+                cout << "There is no book in the list" << endl;
+            }
+        }
+    }
+};
+int main()
+{
+    vector<Book *> obj = {new Book("harry pot to", 3, vector<string>{}), new Book("harry", 4, vector<string>{}), new Book("hello", 5, vector<string>{})};
+    User user1("Bum", vector<Book *>{});
+    User user2("Bem", vector<Book *>{});
+    user1.doBorrow(*obj[0]);
+    user2.doBorrow(*obj[2]);
+    user1.doReturn(*obj[2]);
 }
-	
-	
---
+++ wk8/q1.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <vector>
+
+class Book{
+    private:
+    std::string title;
+    int availableCopies;
+    std::vector<std::string> nameofBorrowers;
+
+    public:
+    Book(){};
+    Book(std::string titleInput, int availableCopiesInput, std::vec)    
+};
+
+class User{
+
+};
+
+int main(int argc, char* argv[]){
+
+}
