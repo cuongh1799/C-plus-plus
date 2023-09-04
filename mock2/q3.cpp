809be8928c73676964cab9928a5cd17ba6d09c89
cuongh1799 <cuongghuynhh1009@gmail.com>
2023-09-04 14:14:25 +0700

    implemented shop system

--- mock2/q3.cpp
+++ mock2/q3.cpp
@@ -2,6 +2,7 @@
 #include <vector>
 #include <string>
 #define SIZE 2
+#define shopSIZE 3
 
 // Shop's product must have its name and unit price recorded
 class Product{
@@ -33,11 +34,6 @@ class Shop{
     }
 };
 
-class BoughtProduct{
-    public:
-    int orderID;
-    std::string Product_name;
-};
 
 // Account must have name and list of order
 // Info of order must have order_id start from 1, list of bought product, total expense
@@ -50,25 +46,64 @@ class Customer_Account{
     std::string name;
     std::string AccountType;
     std::string password;
+    std::vector<Product> Cart;
 
     Customer_Account(std::string nameInput = "", std::string passwordInput = "", std::string AccountTypeInput = "")
 : name(nameInput), password(passwordInput), AccountType(AccountTypeInput){};
+
+    void addtoCart(Product productName){
+        if(AccountType == "Gold"){
+            Product temp = productName;
+            temp.price -= temp.price * 5 / 100;
+            Cart.push_back(temp);
+        }
+        else if (AccountType == "Normal"){
+            Cart.push_back(productName);
+        }
+    }
+
+    // void removefromCart(Product productName){
+    //     for(int i = 0; i < Cart.size(); i++){
+    //         if(Cart[i].name == productName.name){
+    //             Cart.erase(Cart.begin() + i);
+    //             break;
+    //         }
+    //     }
+    // }
+
+    void showCartList(){
+        for(int i = 0; i < Cart.size(); i++){
+            std::cout << i+1 << ". " << Cart[i].name << ", price: " << Cart[i].price << std::endl;
+        }
+    }
 };
 
 int main(int argc, char* argv[]){
     Product product1("Apple", 10000);
     Product product2("Banana", 20000);
-    Shop shop1("Agatha", {});
-    Shop shop2("Valhalla", {});
-    shop1.addProduct(product1);
-    shop1.addProduct(product2);
-    shop2.addProduct(product1);
+    Product product3("Spicy Noodle", 30000);
+
+    Shop* shopList = new Shop[shopSIZE];
+
+    shopList[0] = Shop("Agatha", {});
+    shopList[0].addProduct(product1);
+    shopList[0].addProduct(product2);
+
+    shopList[1] = Shop("Valhalla", {});
+    shopList[1].addProduct(product1);
+
+    shopList[2] = Shop("Vinmart", {});
+    shopList[2].addProduct(product1);
+    shopList[2].addProduct(product2);
+    shopList[2].addProduct(product3);
+    
+    
 
     Customer_Account* accountlist = new Customer_Account[SIZE];
     accountlist[0] = Customer_Account("John", "testing123", "Gold");
     accountlist[1] = Customer_Account("Wick", "123testing", "Normal");
 
-    // LINE 71 - 107: LOGIN SYSTEM
+    // LINE 93 - 122: LOGIN SYSTEM
     std::cout << "Please enter your Username: ";
     std::string usernameInput;
     int accountID = 0;
@@ -100,10 +135,37 @@ int main(int argc, char* argv[]){
     std::cin >> passwordInput;
 
     if(passwordInput.compare(accountlist[accountID].password) == 0){
+        system("cls");
         std::cout << "Signed in successfully!" << std::endl;
         std::cout << "You are now signed in as " << accountlist[accountID].name;
         std::cout << ". You have " << accountlist[accountID].AccountType << " membership!" <<std::endl;
-    } else std::cout << "Sign in failed!";
+    } else {
+        std::cout << "Sign in failed!";
+        return -1;
+    }
 
-    //shop1.listProduct();
+    // SHOP VIEWING SYSTEM
+    int shopchoice;
+    for(int i = 0; i < shopSIZE; i++){
+        std::cout << i+1 << ". " << shopList[i].name << std::endl;
+    }
+    std::cout << "What shop do you want to view ? ";
+    
+    std::cin.ignore(1, '\n');
+    std::cin >> shopchoice;
+    shopchoice--;
+    
+    system("cls");
+    if(shopchoice <= 3 & shopchoice >= 0){
+        std::cout << "You are now viewing " << shopList[shopchoice].name << "!" << std::endl;
+        shopList[shopchoice].listProduct();
+    }
+    else{
+        std::cout << "Error!";
+        return -1;
+    }
+    
+    // BUYING SYSTEM
+    int userchoice;
+    std::cout << "Type 1-3 to add product to cart, 0 to exit, 4 to checkout";
 }
